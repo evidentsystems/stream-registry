615d9b0c4fdec89f97d5313d5cc4a86c9fd59c19
Dave Maughan <dmaughan@expediagroup.com>
2019-07-12 17:09:52 +0100

    more lists

--- graphql-api/src/main/java/com/expediagroup/streamplatform/streamregistry/graphql/model/GraphQLProducer.java
+++ graphql-api/src/main/java/com/expediagroup/streamplatform/streamregistry/graphql/model/GraphQLProducer.java
@@ -15,6 +15,8 @@
  */
 package com.expediagroup.streamplatform.streamregistry.graphql.model;
 
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Value;
 
@@ -25,8 +27,8 @@ public class GraphQLProducer {
   String name;
   String owner;
   String description;
-  Iterable<GraphQLKeyValue> tags;
+  List<GraphQLKeyValue> tags;
   String type;
-  Iterable<GraphQLKeyValue> configuration;
-  Iterable<GraphQLStreamBinding> bindings;
+  List<GraphQLKeyValue> configuration;
+  List<GraphQLStreamBinding> bindings;
 }
